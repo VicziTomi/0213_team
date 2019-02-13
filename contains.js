b51fd68d6ce0cd936391bee27fdef7540fa973ba
VicziTomi <viczian.t@gmail.com>
2019-02-13 15:20:04 +0100

    mi a gebasz

--- contains.js
+++ contains.js
@@ -1,12 +1,12 @@
-let randomArr = require("./generateArray.js");
+let randomArr = require('./generateArray');
 
 const contains = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 90) {
+    if (arr[i] === 4) {
       return true;
-    }    
+    }
   }
   return false;
 };
-console.log(randomArr.arrfill());
-console.log(contains(randomArr.arrfill()));
+console.log(randomArr.generateArray());
+console.log(contains(randomArr.generateArray()));
--- generateArray.js
+++ generateArray.js
@@ -1,10 +1,10 @@
 function generateArray (randomNumber) {
   const tomb = [];
   for (let i = 0; i < 10; i++) {
-    randomNumber = Math.floor((Math.random() * 1000) + 1);
+    randomNumber = Math.floor((Math.random() * 5) + 1);
     tomb.push(randomNumber);
   }
   return tomb;
 }
-console.log(generateArray());
+
 module.exports = { generateArray };
